e1df90e1a802850126d8281ebf79749e04a15158
Guilherme da Silva Gonçalves <guilherme.dasilva@dafiti.com.br>
2021-01-31 21:22:12 -0300

    class diagram

--- src/main/java/com/br/billetAPI/controller/BilletController.java
+++ src/main/java/com/br/billetAPI/controller/BilletController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Controllers {
+public class BilletController {
 
     @Autowired
     private BilletService billetService;
